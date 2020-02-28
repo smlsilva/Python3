3f641870d772e0f42989fedd6e5ac0ad3ea06d25
Samuel <silvasmlsss.02@gmail.com>
2020-02-28 07:12:19 -0300

    Iniciando em Python3

---
+++ inicio.py
@@ -0,0 +1,28 @@
+#todos os comandos em python são uma FUNÇÃO
+print('Olá Mundo!')
+
+print(7 + 4)
+
+print('Olá' , 11) #Para printar string e um Número colocar com VÍRGULA(,)
+
+print('Olá ' + 'Mundo!') #Quando for strings será feito a conctenação
+
+##################################################################################
+
+#Toda Variavel é um Objeto
+'''
+nome = 'Samuel'
+idade = 19
+peso = 79.0
+
+print(nome, idade, peso)
+'''
+
+nome = input('Qual seu Nome ?')
+idade = input('Qual é a sua Idade ?')
+peso = input('Qual é o seu peso ?')
+
+print(nome, idade, peso)
+
+##################################################################################
+
