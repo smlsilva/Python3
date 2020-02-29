c4586df4fe1e26f662c380afca91c14e147d8f65
Samuel <silvasmlsss.02@gmail.com>
2020-02-29 10:01:14 -0300

    Exercicio 1

--- Teste.py
+++ Teste.py
@@ -1,3 +1,12 @@
+'''
 nome = input('Qual é o seu Nome ? ')
 
 print('Seja Bem Vindo ' + nome)
+'''
+
+dia = input('Qual dia que você Nasceu ? ')
+mes = input('Qual Mês que você Nasceu ? ')
+ano = input('Qual Ano que você Nasceu ? ')
+
+print('Você Naceu no ', dia, 'de', mes, 'de', ano,) 
+input('Correto ? ')
