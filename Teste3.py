a337287c8bb9f97845a3e7efbc09131d4e4c2d00
SamuelSilva <47987232+smlsilva@users.noreply.github.com>
2020-03-12 08:28:08 -0300

    Update Teste3.py

--- Teste3.py
+++ Teste3.py
@@ -2,7 +2,7 @@ nome = input("Digite seu Nome: ")
 print("É um Prazer te conhecer, {}!".format(nome))
 
 
-prin(int(7))
-float(7)
-bool(True)
-str("sd")
+print(int(7))
+print(float(7))
+print(bool(True))
+print(str("sd"))
